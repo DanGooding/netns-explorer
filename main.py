2d76d747eee911e9658ccc05049558d8c96746dc
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-05 15:04:26 +0100

    Add tests of parsing

--- main.py
+++ main.py
@@ -41,15 +41,56 @@ class Route:
     interface: Interface
     gateway: Optional[Gateway]
 
-def command_lines(conn: Connection, command: str) -> Generator[str, None, None]:
+def command_lines(conn: Connection, command: str) -> list[str]:
     links_result = conn.run(command, hide='both')
     if links_result.failed:
         raise RuntimeError(links_result)
 
-    for link in links_result.stdout.splitlines():
-        link = link.strip()
-        if link:
-            yield link
+    return links_result.stdout.splitlines()
+
+ip_link_pattern = re.compile(r'^(?P<id>\d+): (?P<name>\w+)(?:@if(?P<pair_id>\d+))?: .*?(?: master (?P<bridge_name>[\w_]+))? state (?P<state>[A-Z]+)')
+def parse_ip_links(lines: list[str], namespace: Namespace) -> dict[int, Interface]:
+    interfaces_by_id = {}
+    for line in lines:
+        match = ip_link_pattern.match(line.strip())
+        if not match:
+            continue
+
+        groups = match.groupdict()
+        interface_id = int(groups['id'])
+        veth_pair_id = None
+        if groups['pair_id']:
+            veth_pair_id = int(groups['pair_id'])
+
+        interfaces_by_id[interface_id] = Interface(
+            namespace=namespace,
+            id=interface_id,
+            name=groups['name'],
+            state=groups['state'],
+            bridge_name=groups['bridge_name'],
+            veth_pair_id=veth_pair_id,
+            address=None)
+
+    return interfaces_by_id
+
+ip_addr_pattern = re.compile(r'^(?P<id>\d+): \w+ +(?:inet (?P<ipv4>[\d.]+)/\d+)?(?:inet6 (?P<ipv6>[:0-9]+)/\d+)?')
+""" parses a the output from `ip addr`
+    returns a dict { interface_id: ipv4addr }
+"""
+def parse_ip_addrs(lines: list[str]) -> dict[int, IPv4Address]:
+    addrs_by_interface_name = {}
+    for line in lines:
+        match = ip_addr_pattern.match(line.strip())
+        if not match:
+            continue
+
+        groups = match.groupdict()
+        if not groups['ipv4']:
+            # ignore ipv6 for now
+            continue
+        addrs_by_interface_name[int(groups['id'])] = IPv4Address(groups['ipv4'])
+
+    return addrs_by_interface_name
 
 @click.command()
 @click.argument('ssh_host')
@@ -57,44 +98,15 @@ def main(ssh_host: str):
     conn = Connection(ssh_host)
 
     default_namespace = Namespace(name='', routing_table=[])
-    interfaces_by_id = {}
-    link_pattern = re.compile(r'(?P<id>\d+): (?P<name>\w+)(?:@if(?P<pair_id>\d+))?: (?:.+ )?(?:master (?P<bridge_name>\w+))?(?: .*)?state (?P<state>[A-Z]+)')
-    for link in command_lines(conn, 'ip -detail -oneline link'):
-        match = link_pattern.match(link).groupdict()
+    interfaces_by_id = parse_ip_links(
+        command_lines(conn, 'ip -detail -oneline link'),
+        namespace=default_namespace)
 
-        id = match['id']
-        interface = Interface(
-            namespace=default_namespace,
-            id=match['id'],
-            name=match['name'],
-            state=match['state'],
-            bridge_name=match['bridge_name'],
-            veth_pair_id=match['pair_id'],
-            address=None)
-
-        interfaces_by_id[id] = interface
-
-    address_pattern = re.compile(r'(?P<id>\d+): \w+ +(?:inet (?P<ipv4>[\d.]+)/\d+)?(?:inet6 (?P<ipv6>[:0-9]+)/\d+)?')
-    for address in command_lines(conn, 'ip -oneline address'):
-        match = address_pattern.match(address).groupdict()
-        if not match['ipv4']:
-            # ignore ipv6 for now
-            continue
-        interfaces_by_id[match['id']].address = IPv4Address(match['ipv4'])
+    for interface_id, ip_addr in parse_ip_addrs(command_lines(conn, 'ip -oneline address')).items():
+        interfaces_by_id[interface_id].address = ip_addr
 
     for interface in interfaces_by_id.values():
         print(interface)
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
