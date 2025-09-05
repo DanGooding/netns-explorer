75a58a3b557912e7a6babdd9b2e2701c4b537580
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-05 12:24:57 +0100

    Retrieve and parse interface info

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import click
 from dataclasses import dataclass
 from ipaddress import IPv4Address, IPv4Network
-from typing import Optional
+from fabric import Connection
+import re
+from typing import Optional, Generator
 
 @dataclass
 class Namespace:
     name: str
-    routingTable: list['Route']
+    routing_table: list['Route']
 
 @dataclass
 class Interface:
@@ -15,18 +18,20 @@ class Interface:
     name: str
     """ the human-readable name - meaningful within the namespace """
 
-    address: IPv4Address
+    address: Optional[IPv4Address]
+    state: str
+    """ UP/DOWN etc. """
 
-    kind: str
-    """ veth, bridge, vxlan, etc. """
-    bridgeId: Optional[int]
-    """ for an interface connected to a bridge (virtual switch) """
-    vethPairId: Optional[int]
+    bridge_name: Optional[str]
+    """ For an interface connected to a bridge (virtual switch).
+        The bridge will be in the same namespace. 
+    """
+    veth_pair_id: Optional[int]
     """ the paired interface, if this is a veth """
 
 @dataclass
 class Gateway:
-    """ a router - provides routing beyond this network """
+    """ a router - provides routing beyond this subnet """
     address: IPv4Address
 
 @dataclass
@@ -36,41 +41,61 @@ class Route:
     interface: Interface
     gateway: Optional[Gateway]
 
+def command_lines(conn: Connection, command: str) -> Generator[str, None, None]:
+    links_result = conn.run(command, hide='both')
+    if links_result.failed:
+        raise RuntimeError(links_result)
 
-# fabric for running commands remotely
-# + cli library (click)
-# + graphviz
-# types
+    for link in links_result.stdout.splitlines():
+        link = link.strip()
+        if link:
+            yield link
+
+@click.command()
+@click.argument('ssh_host')
+def main(ssh_host: str):
+    conn = Connection(ssh_host)
+
+    default_namespace = Namespace(name='', routing_table=[])
+    interfaces_by_id = {}
+    link_pattern = re.compile(r'(?P<id>\d+): (?P<name>\w+)(?:@if(?P<pair_id>\d+))?: (?:.+ )?(?:master (?P<bridge_name>\w+))?(?: .*)?state (?P<state>[A-Z]+)')
+    for link in command_lines(conn, 'ip -detail -oneline link'):
+        match = link_pattern.match(link).groupdict()
+
+        id = match['id']
+        interface = Interface(
+            namespace=default_namespace,
+            id=match['id'],
+            name=match['name'],
+            state=match['state'],
+            bridge_name=match['bridge_name'],
+            veth_pair_id=match['pair_id'],
+            address=None)
+
+        interfaces_by_id[id] = interface
+
+    address_pattern = re.compile(r'(?P<id>\d+): \w+ +(?:inet (?P<ipv4>[\d.]+)/\d+)?(?:inet6 (?P<ipv6>[:0-9]+)/\d+)?')
+    for address in command_lines(conn, 'ip -oneline address'):
+        match = address_pattern.match(address).groupdict()
+        if not match['ipv4']:
+            # ignore ipv6 for now
+            continue
+        interfaces_by_id[match['id']].address = IPv4Address(match['ipv4'])
+
+    for interface in interfaces_by_id.values():
+        print(interface)
 
 
-# args:
-# - ssh host (else run locally)
-# - namespace source files (e.g. the docker one)
-
-# rendering the model
-# nodes: (style kinds differently)
-# - interface
-#   - name
-#   - ip
-# - routing table, html table of
-#   - route
-#     - cidr
-# - gateway
-#   - ip
-# edges: (style different kinds differently)
-# - veth<>veth
-# - route->interface
-# - bridge->child
-# subgraphs:
-# - namespace
-#   but not the default namespace?
 
 
-# operation:
-# - for each namespace
-#   - run and parse each command
-#   - update our model with info
-# - render our model to a graph
 
-# metadata:
-# - link namespaces to docker containers somehow?
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
