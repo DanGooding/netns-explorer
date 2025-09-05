f722bd62181c970ce237d90ebf9082ef7ea223f9
Dan Gooding <dan.gooding2000@gmail.com>
2025-09-05 17:17:30 +0100

    Render namespace subgraphs

--- render.py
+++ render.py
@@ -19,21 +19,27 @@ def render(namespaces: Iterable[model.Namespace]) -> graphviz.Digraph:
             interfaces_by_namespace_and_name[namespace.metadata.path][interface.name] = interface
 
     for namespace in namespaces:
-        for interface in namespace.interfaces:
-            dot.node(interface_node_name(interface), interface.name)
+        if namespace.is_default():
+            subgraph_name = ''
+        else:
+            subgraph_name = f'cluster:{namespace.metadata.path}'
+        with dot.subgraph(None, subgraph_name) as ns_graph:
+            for interface in namespace.interfaces:
+                ns_graph.node(interface_node_name(interface), interface.name)
+                ns_graph.attr(color='grey')
 
-            if interface.veth_pair_id:
-                # undirected veth pair edge
-                dot.edge(
-                    interface_node_name(interface),
-                    interface_id_node_name(interface.veth_pair_id))
+                if interface.veth_pair_id:
+                    # undirected veth pair edge
+                    dot.edge(
+                        interface_node_name(interface),
+                        interface_id_node_name(interface.veth_pair_id))
 
-            if interface.bridge_name:
-                # directed bridge switch->interface edge
+                if interface.bridge_name:
+                    # directed bridge switch->interface edge
 
-                bridge_interface = interfaces_by_namespace_and_name[namespace.metadata.path][interface.bridge_name]
-                dot.edge(
-                    interface_node_name(bridge_interface),
-                    interface_node_name(interface))
+                    bridge_interface = interfaces_by_namespace_and_name[namespace.metadata.path][interface.bridge_name]
+                    dot.edge(
+                        interface_node_name(bridge_interface),
+                        interface_node_name(interface))
 
     return dot
